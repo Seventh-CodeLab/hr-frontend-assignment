2313deb688541b2bc632deb56455df957c7309b7
Krister Emanuelsen <SeventhDisaster@outlook.com>
2021-09-02 20:41:22 +0200

    feat: filtering on search result, no more dangerouslysetinnerhtml, reduced accordion content

--- src/components/SearchResultItem.tsx
+++ src/components/SearchResultItem.tsx
@@ -9,10 +9,11 @@ const SearchResultItem = ({shipId, heading, body, yearOfConstruction, shipyard, 
         setExpanded(!expanded)
     }
 
-    function createMarkup() {
-
-        return {__html: body ? body : "Nothing"};
-      }
+    // Converting the HTML formatted response from the body to a plaintext format for display
+    const toShortenedPlainText = (body : string) => {
+        body = body.replaceAll(/&([a-z0-9]+|#[0-9]{1,6}|#x[0-9a-fA-F]{1,6});/ig, ""); // Regex filters out HTML Entities
+        return body.substring(3, body.indexOf("</p>"));
+    }
 
     return (
         <>
@@ -24,7 +25,7 @@ const SearchResultItem = ({shipId, heading, body, yearOfConstruction, shipyard, 
                         viewBox="0 0 16 16" 
                         fill="currentColor" 
                         xmlns="http://www.w3.org/2000/svg">
-                        <g clip-path="url(#clip0)">
+                        <g clipPath="url(#clip0)">
                             <path fillRule="evenodd" clipRule="evenodd"
                             d="M15.8536 11.8536C15.6583 12.0488 15.3417 12.0488 15.1464 11.8536L8 4.70711L0.853554 11.8536C0.658291 12.0488 0.341709 12.0488 0.146446 11.8536C-0.0488167 11.6583 -0.0488166 11.3417 0.146446 11.1464L7.64645 3.64645C7.84171 3.45118 8.15829 3.45118 8.35355 3.64645L15.8536 11.1464C16.0488 11.3417 16.0488 11.6583 15.8536 11.8536Z" />
                         </g>
@@ -36,16 +37,21 @@ const SearchResultItem = ({shipId, heading, body, yearOfConstruction, shipyard, 
             </ItemContainer>
             {
                 expanded &&
-                
-                // NOTE: I would ***NEVER*** actually use this type of XSS injection prone method in a real environment.
-                <ExpansionContent dangerouslySetInnerHTML={createMarkup()}></ExpansionContent>
+                <ExpansionContent>
+                    <h4>{heading}</h4>
+                    <p>{beds} beds</p>
+                    <p>Shipyard: {shipyard}</p>
+                    <p>
+                        {toShortenedPlainText(body!)}
+                    </p>
+                </ExpansionContent>
             }
             <Divider/>
         </>
     )
 }
 
-const ItemContainer = styled.tr`
+const ItemContainer = styled.div`
     width: 90%;
     display: flex;
     flex-direction: row;
@@ -57,11 +63,11 @@ const ItemContainer = styled.tr`
     }
 `
 
-const ItemExpansion = styled.td`
+const ItemExpansion = styled.div`
     width: 3em;
 `
 
-const ItemField = styled.td`
+const ItemField = styled.div`
     width: 30%;
 `
 
@@ -71,7 +77,7 @@ const Divider = styled.hr`
 `
 
 const ExpansionContent = styled.div`
-    margin: 2em;
+    margin: 0 2em;
 `
 
 export default SearchResultItem
--- src/components/SearchResultList.tsx
+++ src/components/SearchResultList.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 import IShip from '../models/IShip';
 import SearchResultItem from './SearchResultItem';
@@ -8,24 +8,83 @@ type SearchResultListProps = {
 }
 
 const SearchResultList = (props : SearchResultListProps) => {
+    const [sortedBy, setSortedBy] = useState<string>()
+
+    const sortBy = (byValue : string) => {
+        switch (byValue) {
+            case "name":
+                props.list.sort((l, r) => {
+                    if(sortedBy === "name") {
+                        return l.heading < r.heading ? 1 : -1;
+                    } 
+                    return l.heading > r.heading ? 1 : -1
+                }) 
+                sortedBy === "name" ? setSortedBy("namedesc") : setSortedBy("name");
+                break;
+
+            case "year": 
+                sortedBy === "year" ?
+                props.list.sort((l, r) => {
+                    if(!l.yearOfConstruction || !r.yearOfConstruction) return 1
+                    return parseInt(l.yearOfConstruction) > parseInt(r.yearOfConstruction) ? 1 : -1
+                })
+                : 
+                props.list.sort((l, r) => {
+                    if(!l.yearOfConstruction || !r.yearOfConstruction) return 1
+                    return parseInt(l.yearOfConstruction) > parseInt(r.yearOfConstruction) ? -1 : 1
+                })
+                sortedBy === "year" ? setSortedBy("yeardesc") : setSortedBy("year");
+                break;
+
+            case "capacity":
+                sortedBy === "capacity" ?
+                props.list.sort((l, r) => {
+                    if(!l.passengerCapacity || !r.passengerCapacity) return 1
+                    return parseInt(l.passengerCapacity) > parseInt(r.passengerCapacity) ? 1 : -1
+                })
+                : 
+                props.list.sort((l, r) => {
+                    if(!l.passengerCapacity || !r.passengerCapacity) return 1
+                    return parseInt(l.passengerCapacity) < parseInt(r.passengerCapacity) ? 1 : -1
+                })
+                sortedBy === "capacity" ? setSortedBy("capacitydesc") : setSortedBy("capacity");
+                break;
+            default:
+                break;
+        }
+    }
+
     return (
         <ListContainer>
             <TableHeader>
                 <ItemExpansion/>
-                <ItemField> Name: </ItemField>
-                <ItemField> Year: </ItemField>
-                <ItemField> Capacity: </ItemField>
+                <ItemField onClick={() => {
+                    sortBy("name")
+                }}> Name: </ItemField>
+                <ItemField onClick={() => {
+                    sortBy("year")
+                }}> Year: </ItemField>
+                <ItemField onClick={() => {
+                    sortBy("capacity")
+                }}> Capacity: </ItemField>
             </TableHeader>
+            <hr style={{width: "90%", border: "solid lightgray 1px"}}/>
             {
                 props.list.map(ship => {
                     return  <SearchResultItem key={ship.shipId} {...ship}/>
                 })
             }
+            {
+                props.list.length === 0 &&
+                <div>
+                    <NoResults>No results</NoResults>
+                </div>
+            }
         </ListContainer>
     )
 }
 
-const ListContainer = styled.table`
+const ListContainer = styled.div`
     background-color: #fafafa;
     width: 60%;
     margin: 2em 10em;
@@ -35,7 +94,7 @@ const ListContainer = styled.table`
     align-items: center;
 `
 
-const TableHeader = styled.tr`
+const TableHeader = styled.div`
     width: 90%;
     display: flex;
     flex-direction: row;
@@ -44,12 +103,22 @@ const TableHeader = styled.tr`
     font-weight: bold;
 `
 
-const ItemExpansion = styled.td`
+const ItemExpansion = styled.div`
     width: 3em;
 `
 
-const ItemField = styled.td`
+const ItemField = styled.div`
     width: 30%;
+    user-select: none;
+    transition: .1s ease;
+    :hover {
+        cursor: pointer;
+        color: #5555ff
+    }
+`
+
+const NoResults = styled.h3`
+    color: #ababab;
 `
 
 export default SearchResultList
